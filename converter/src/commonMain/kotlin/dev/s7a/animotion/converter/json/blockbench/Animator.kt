385bbe2dc625c512fd9b4289f3174ad915e7667d
sya-ri <git@s7a.dev>
2023-06-03 04:06:52 +0900

    feat: Animator.Type を追加

--- converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Animator.kt
+++ converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Animator.kt
@@ -1,10 +1,17 @@
 package dev.s7a.animotion.converter.json.blockbench
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Animator(
     val name: String, // TODO Outliner#name と同じ
-    val type: String, // TODO bone であることをチェック
+    val type: Type,
     val keyframes: List<Keyframes>,
-)
+) {
+    @Serializable
+    enum class Type {
+        @SerialName("bone")
+        Bone,
+    }
+}
