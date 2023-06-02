c084683d543e6fc9effda31603c4557b27f96532
sya-ri <git@s7a.dev>
2023-06-03 04:05:39 +0900

    feat: Meta.ModelFormat を追加

--- converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Meta.kt
+++ converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Meta.kt
@@ -6,5 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Meta(
     @SerialName("format_version") val formatVersion: String, // TODO 4.5 であることをチェックする
-    @SerialName("model_format") val modelFormat: String, // TODO free であることをチェックする
-)
+    @SerialName("model_format") val modelFormat: ModelFormat,
+) {
+    @Serializable
+    enum class ModelFormat {
+        @SerialName("free")
+        Free,
+    }
+}
