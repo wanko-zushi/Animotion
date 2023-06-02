e9bbb4999d6742538de19da8db88f7714c1d114d
sya-ri <git@s7a.dev>
2023-06-03 04:18:17 +0900

    feat: BlockBenchModel#elements のクラス名を変更・Element.Type の追加

--- converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/BlockBenchModel.kt
+++ converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/BlockBenchModel.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class BlockBenchModel(
     val meta: Meta, // TODO モデルをロードする前に Meta をチェックして必要に応じてローダーを変える
     val resolution: Resolution, // TODO MinecraftModel: texture_size
-    val elements: List<Elements>,
+    val elements: List<Element>,
     val outliner: List<Outliner>, // TODO このグループ単位で MinecraftModel を生成
     val textures: List<Textures>, // TODO このデータを assets/animotion/textures に出力
     val animations: List<Animations>,
--- converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Element.kt
+++ converter/src/commonMain/kotlin/dev/s7a/animotion/converter/json/blockbench/Element.kt
@@ -1,14 +1,22 @@
 package dev.s7a.animotion.converter.json.blockbench
 
+import dev.s7a.animotion.converter.json.common.FaceType
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.uuid.UUID
 
 @Serializable
-data class Elements(
+data class Element(
     val from: List<Double>, // TODO MinecraftModel#Elements: from
     val to: List<Double>, // TODO MinecraftModel#Elements: to
     val origin: List<Double>, // TODO MinecraftModel#Elements: origin
-    val faces: Map<Face.Type, Face>, // TODO MinecraftModel#Elements: faces
-    val type: String, // TODO cube であることをチェックする
+    val faces: Map<FaceType, Face>, // TODO MinecraftModel#Elements: faces
+    val type: Type,
     val uuid: UUID,
-)
+) {
+    @Serializable
+    enum class Type {
+        @SerialName("cube")
+        Cube,
+    }
+}
