9b204639c9043969565ba424f8acfbc292137448
sya-ri <git@s7a.dev>
2023-11-08 12:13:03 +0900

    chore: 実行可能ファイルを生成するようにした

--- converter/build.gradle.kts
+++ converter/build.gradle.kts
@@ -1,14 +1,61 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
+
 plugins {
     kotlin("plugin.serialization") version "1.9.20"
     id("org.jetbrains.kotlinx.kover") version "0.7.4"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 kotlin {
-    jvm()
-    linuxX64()
-    mingwX64()
-    macosX64()
-    macosArm64()
+    jvm {
+        @Suppress("OPT_IN_USAGE")
+        mainRun {
+            mainClass.set("dev.s7a.animotion.converter.MainKt")
+        }
+
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+
+        registerShadowJar("animotion-converter", "dev.s7a.animotion.converter.MainKt")
+    }
+
+    linuxX64 {
+        binaries {
+            executable {
+                baseName = "animotion-converter"
+                entryPoint("dev.s7a.animotion.converter.main")
+            }
+        }
+    }
+
+    mingwX64 {
+        binaries {
+            executable {
+                baseName = "animotion-converter"
+                entryPoint("dev.s7a.animotion.converter.main")
+            }
+        }
+    }
+
+    macosX64 {
+        binaries {
+            executable {
+                baseName = "animotion-converter"
+                entryPoint("dev.s7a.animotion.converter.main")
+            }
+        }
+    }
+
+    macosArm64 {
+        binaries {
+            executable {
+                baseName = "animotion-converter"
+                entryPoint("dev.s7a.animotion.converter.main")
+            }
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -23,6 +70,31 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+/**
+ * https://stackoverflow.com/questions/63426211/kotlin-multiplatform-shadowjar-gradle-plugin-creates-empty-jar
+ */
+fun KotlinJvmTarget.registerShadowJar(baseName: String, mainClassName: String) {
+    val targetName = name
+    compilations.named("main") {
+        tasks {
+            val shadowJar = register<ShadowJar>("${targetName}ShadowJar") {
+                group = "build"
+                from(output)
+                configurations = listOf(runtimeDependencyFiles)
+                archiveFileName.set("$baseName.jar")
+                manifest {
+                    attributes("Main-Class" to mainClassName)
+                }
+                mergeServiceFiles()
+            }
+
+            getByName("${targetName}Jar") {
+                finalizedBy(shadowJar)
             }
         }
     }
