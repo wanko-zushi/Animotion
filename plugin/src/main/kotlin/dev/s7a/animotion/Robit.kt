f30eb2e59e6c2a84864c4955176c22ae3e4db56f
sya-ri <git@s7a.dev>
2025-02-05 04:32:30 +0900

    feat(api): Add AnimotionPart.Model.Both

--- plugin/src/main/kotlin/dev/s7a/animotion/Robit.kt
+++ plugin/src/main/kotlin/dev/s7a/animotion/Robit.kt
@@ -5,12 +5,12 @@ import org.bukkit.Material
 class Robit(
     animotion: Animotion,
 ) : AnimotionModel(animotion) {
-    val gear = part(Material.KELP, 1, Position(0.0, 1.4843749999999996, 0.0), Rotation(0.0, -0.3927, 0.0))
-    val body = part(Material.KELP, 2)
-    val leftShoulder = part(Material.KELP, 3, Position(-0.375, 0.90625, 0.0))
-    val leftArm = part(Material.KELP, 4, Position(-0.375, 0.90625, 0.0))
-    val rightShoulder = part(Material.KELP, 5, Position(0.375, 0.90625, 0.0))
-    val rightArm = part(Material.KELP, 6, Position(0.375, 0.90625, 0.0))
-    val leftLeg = part(Material.KELP, 7, Position(-0.125, 0.3958333333333333, 0.0))
-    val rightLeg = part(Material.KELP, 8, Position(0.125, 0.3958333333333333, 0.0))
+    val gear = part("animotion:robit/gear", Material.KELP, 1, Position(0.0, 1.4843749999999996, 0.0), Rotation(0.0, -0.3927, 0.0))
+    val body = part("animotion:robit/body", Material.KELP, 2)
+    val leftShoulder = part("animotion:robit/left_shoulder", Material.KELP, 3, Position(-0.375, 0.90625, 0.0))
+    val leftArm = part("animotion:robit/left_arm", Material.KELP, 4, Position(-0.375, 0.90625, 0.0))
+    val rightShoulder = part("animotion:robit/right_shoulder", Material.KELP, 5, Position(0.375, 0.90625, 0.0))
+    val rightArm = part("animotion:robit/right_arm", Material.KELP, 6, Position(0.375, 0.90625, 0.0))
+    val leftLeg = part("animotion:robit/left_leg", Material.KELP, 7, Position(-0.125, 0.3958333333333333, 0.0))
+    val rightLeg = part("animotion:robit/right_leg", Material.KELP, 8, Position(0.125, 0.3958333333333333, 0.0))
 }
