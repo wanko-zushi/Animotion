cb908ad1b19107543633ba64838b247ec814c6f3
sya-ri <git@s7a.dev>
2025-02-12 23:46:46 +0900

    test(converter): Improve assertFileContent

--- converter/src/test/kotlin/util/AssertFileContent.kt
+++ converter/src/test/kotlin/util/AssertFileContent.kt
@@ -2,10 +2,22 @@ package util
 
 import java.io.File
 import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 fun assertFileContent(
     expected: File,
     actual: File,
 ) {
-    assertContentEquals(expected.readBytes(), actual.readBytes())
+    when (val extension = expected.extension) {
+        "json", "mcmeta" -> {
+            assertEquals(expected.readText(), actual.readText(), actual.name)
+        }
+        "png" -> {
+            assertContentEquals(expected.readBytes(), actual.readBytes(), actual.name)
+        }
+        else -> {
+            fail("Unsupported file extension $extension")
+        }
+    }
 }
