1e5557d5366d536a95d48aa90b0c46f4a89a8959
sya-ri <git@s7a.dev>
2026-02-11 02:01:21 +0900

    refactor(build): Replace custom publishing configuration with Maven Publish Plugin

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
-    signing
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
+    alias(libs.plugins.mavenPublish)
 }
 
 dependencies {
@@ -23,4 +26,35 @@ kotlin {
     }
 }
 
-applyPublishingConfig("animotion-api")
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("dev.s7a", "animotion-api", version.toString())
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationJavadoc"),
+            sourcesJar = true,
+        ),
+    )
+    pom {
+        name.set("animotion-api")
+        description.set("Spigot library and CLI tool to animate BlockBench models in Minecraft Java Edition")
+        url.set("https://github.com/wanko-zushi/Animotion")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/wanko-zushi/Animotion/blob/master/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("sya-ri")
+                name.set("sya-ri")
+                email.set("contact@s7a.dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/wanko-zushi/Animotion")
+        }
+    }
+}
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
-    signing
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
+    alias(libs.plugins.mavenPublish)
 }
 
 java {
@@ -17,4 +20,35 @@ kotlin {
     }
 }
 
-applyPublishingConfig("animotion-common")
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("dev.s7a", "animotion-common", version.toString())
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationJavadoc"),
+            sourcesJar = true,
+        ),
+    )
+    pom {
+        name.set("animotion-common")
+        description.set("Spigot library and CLI tool to animate BlockBench models in Minecraft Java Edition")
+        url.set("https://github.com/wanko-zushi/Animotion")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/wanko-zushi/Animotion/blob/master/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("sya-ri")
+                name.set("sya-ri")
+                email.set("contact@s7a.dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/wanko-zushi/Animotion")
+        }
+    }
+}
